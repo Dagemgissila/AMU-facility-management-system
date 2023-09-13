cae92ceff4c7efcc56260bea247abddf5e4b81fa
Dagemgissila <dagemgisila@gmail.com>
2023-09-13 21:20:00 +1100

    redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
+        $user=User::count();
+        if($user<1){
+            return redirect()->route('admin.register');
+        }
         return view("auth.home");
     }
 }
